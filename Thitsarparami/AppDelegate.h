40a8266f05227a7bb040586ae7e56385c2d0ece6
Thaw Hein Thit <thawheinthit@gmail.com>
2015-12-13 13:52:20 +0800

    Initial Commit

---
+++ Thitsarparami/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Thitsarparami
+//
+//  Created by Thaw Hein Thit on 13/12/15.
+//  Copyright © 2015 Thaw Hein Thit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Thitsarparami/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Thitsarparami
+//
+//  Created by Thaw Hein Thit on 13/12/15.
+//  Copyright © 2015 Thaw Hein Thit. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
